9a0dab067eca0c63c2032446da84517c8f2cb594
oldVinyl <neamewu@gmail.com>
2025-10-22 11:47:40 +0000

    completed post ad page

--- src/pages/PostAdPage.tsx
+++ src/pages/PostAdPage.tsx
@@ -1,17 +1,27 @@
+import MobileBanner from '../components/MobileBanner';
 import PageLocked from '../components/PageLocked'
 import ProfileStats from '../components/ProfileStats'
+import MenuButton from "../components/MenuButton";
 
 function PostAdPage() {
   return (
-    <div className="flex flex-col lg:flex-row w-full h-screen bg-[#f3f4f6] -mt-3 overflow-hidden">
+    <div className="flex flex-col lg:flex-row w-full h-screen bg-[var(--div-active)] text-[var(--dark-def)] overflow-hidden">
 
-      <div className="w-full lg:w-[25%] p-2.5 mt-1 flex-shrink-0 overflow-y-auto">
+      <div className="sm:hidden w-full">
+        <MobileBanner backto="Home" page="Post An Ad"/>
+      </div>
+
+      {/* Profile sidebar (desktop only) hidden on mobile */}
+      <div className="hidden sm:flex w-[25vw] h-[100vh] items-center justify-center pl-2">
         <ProfileStats />
       </div>
 
+      {/* hidden on mobile */}
       <div className="flex items-center justify-center w-full lg:flex-grow p-4">
         <PageLocked page="Post Ad" />
       </div>
+
+      <MenuButton />
     </div>
   )
 }
