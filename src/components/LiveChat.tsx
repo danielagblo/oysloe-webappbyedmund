1b4b7931777d8d19a49746f4a5289a97908ee592
fireheart071 <edmundamonoo@gmail.com>
2025-10-23 15:29:02 +0000

    inbox change

--- src/components/LiveChat.tsx
+++ src/components/LiveChat.tsx
@@ -25,22 +25,32 @@ export default function LiveChat({ caseId, onClose }: LiveChatProps) {
 
   return (
     <div className="flex h-full border-gray-100 ">
-      <div className="relative rounded-2xl bg-white px-5 py-3 h-full w-full flex flex-col">
+      <div className="relative rounded-2xl bg-white px-4 py-3 h-full w-full flex flex-col">
         <button className="absolute right-1 block sm:hidden" onClick={onClose}><img src="/close.svg" alt="" className="p-2" /></button>
-        <div className="flex-1 p-6 overflow-y-auto space-y-4">
+        <div className="flex-1 p-3 overflow-y-auto space-y-6">
           <p className="text-xs text-gray-400 text-center mb-6">Yesterday</p>
-          {/* Hardcoded messages for now */}
+
+          {/* Received message */}
           <div className="flex justify-start">
-            <div className="bg-gray-100 p-3 rounded-xl rounded-tl-none">
-              <p className="text-sm">Hi, can I grab your product?</p>
+            <div className="flex flex-col items-start">
+              <div className="inline-flex items-center  z-20 -mb-2 gap-2"><img src="/face.svg" alt="User" className="w-8 h-8 rounded-full" /><p className="text-sm inline">User</p></div>
+              <div className="border border-gray-200 p-3 rounded-xl rounded-tl-none">
+                <p className="text-sm">Hi, can I grab your product?</p>
+              </div>
             </div>
           </div>
+
+          {/* Sent message */}
           <div className="flex justify-end">
-            <div className="bg-green-100 text-black p-3 rounded-xl rounded-tr-none">
-              <p className="text-sm">Sure! Which one?</p>
+            <div className="flex flex-col items-end">
+              <div className="inline-flex items-center  z-20 -mb-2 gap-2"><p className="text-sm inline">Me</p><img src="/face.svg" alt="Me" className=" w-8 h-8 rounded-full" /></div>
+              <div className="border border-gray-200 bg-green-100 text-black p-3 rounded-xl rounded-tr-none">
+                <p className="text-sm">Sure! Which one?</p>
+              </div>
             </div>
           </div>
         </div>
+
 
         <ChatInput />
       </div>
