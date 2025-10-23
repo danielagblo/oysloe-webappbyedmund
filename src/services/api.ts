f05b39b33f22816e597fec111fd67f437fc9d836
fireheart071 <edmundamonoo@gmail.com>
2025-10-23 14:57:49 +0000

    fixing styling

---
+++ src/services/api.ts
@@ -0,0 +1,19 @@
+const API_URL = import.meta.env.VITE_API_URL;
+import type { RegisterUser } from "../types/api";
+
+
+export async function registerUser(userData: RegisterUser) {
+  const response = await fetch(`${API_URL}/register`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(userData)
+  });
+
+  if (!response.ok) {
+    const error = await response.text();
+    throw new Error(error);
+  }
+
+  return response.json();
+}
+
---
+++ src/types/api.ts
@@ -0,0 +1,8 @@
+export interface RegisterUser {
+  email: string;
+  phone: string;
+  password: string;
+  name: string;
+  address: string;
+  referral_code: string;
+}
