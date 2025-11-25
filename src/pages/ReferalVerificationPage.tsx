3ec9043c1087da17c524a89dbaef8875494347b3
oldVinyl <neamewu@gmail.com>
2025-11-25 15:53:11 +0000

    changes

--- src/pages/ReferalVerificationPage.tsx
+++ src/pages/ReferalVerificationPage.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import useIsSmallScreen from "../Auth/useIsSmallScreen";
+import useIsSmallScreen from "../hooks/useIsSmallScreen";
 import Button from "../components/Button";
 import OnboardingScreen from "../components/OnboardingScreen";
 
