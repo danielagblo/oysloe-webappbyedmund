2378267f97b1409e0cdf80989c3d449c1d70323f
fireheart071 <edmundamonoo@gmail.com>
2025-11-27 12:22:09 +0000

    fix: clean up formatting in DebuggerButton component

--- src/components/DebuggerButton.tsx
+++ src/components/DebuggerButton.tsx
@@ -1,15 +1,15 @@
-function DebuggerButton ( { title, data } : { title?: string, data: unknown } ) {
+function DebuggerButton({ title, data }: { title?: string, data: unknown }) {
   return (
     <>
       {/* button for debugging. i'll remove when im done */}
-      <button 
+      <button
         name="sexy button. most useful button."
-        className="fixed z-50 top-10 left-10 bg-pink-200 p-3 cursor-pointer" 
+        className="fixed z-50 top-10 left-10 bg-pink-200 p-3 cursor-pointer hidden"
         onClick={() => console.log(title, ": ", data)}
       >
         {title}
       </button>
-      </>
+    </>
   )
 }
 
