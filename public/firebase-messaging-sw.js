ffd4e74d9d76f37faa6b8942aa6eea01fd074e01
oldVinyl <neamewu@gmail.com>
2025-12-22 12:23:07 +0000

    chore(.env): remove .env refactor(firebase-messaging-sw.js): format firebase initialization to remove env vars

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,7 +1,13 @@
 importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js');
 
-firebase.initializeApp({"apiKey":"","authDomain":"","projectId":"","messagingSenderId":"","appId":""});
+firebase.initializeApp({
+  "apiKey":"",
+  "authDomain":"",
+  "projectId":"",
+  "messagingSenderId":"",
+  "appId":""
+});
 
 const messaging = firebase.messaging();
 
